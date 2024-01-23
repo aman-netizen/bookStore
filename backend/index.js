6036e60c7be1bd782ab18ebfda8d9fbf3ad7478f
Aman Yadav <amanyadav04st@gmail.com>
2024-01-23 20:12:13 +0530

    production update

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,15 @@ app.use(express.json());
 app.use("/api/user", userRoute); 
 app.use("/api/book", bookRoute);
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
+  });
+}
+
 // listing on ports
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT} `);
