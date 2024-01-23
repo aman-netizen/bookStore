f831c9376e4d8d4c8c596919a8ac681e31d3fc39
Aman Yadav <amanyadav04st@gmail.com>
2024-01-23 20:37:13 +0530

    fixed issue

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import userRoute from './routes/userRoute.js'
 import bookRoute from './routes/bookRoute.js'
 import connect from "./config/db.js";
-import path from 'path'
 import { config } from "dotenv";
 const app = express();
 config({
@@ -23,15 +22,6 @@ app.use(express.json());
 // handling routes
 app.use("/api/user", userRoute); 
 app.use("/api/book", bookRoute);
-
-
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("frontend/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
-  });
-}
 
 // listing on ports
 app.listen(PORT, () => {
