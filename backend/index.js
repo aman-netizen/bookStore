8b8b16d4e47a7f589c1262439dd8dc8b3ca2b146
Aman Yadav <amanyadav04st@gmail.com>
2024-02-21 09:42:36 +0530

    api changes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import userRoute from './routes/userRoute.js'
 import bookRoute from './routes/bookRoute.js'
 import connect from "./config/db.js";
 import { config } from "dotenv";
+import path from "path"
 const app = express();
 config({
   path: "./.env",
@@ -22,6 +23,20 @@ app.use(express.json());
 // handling routes
 app.use("/api/user", userRoute); 
 app.use("/api/book", bookRoute);
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // listing on ports
 app.listen(PORT, () => {
